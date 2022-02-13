688c1b50699b6858f17d845d46a0ad94ca665354
Evertton <evertton.martins@gmail.com>
2022-02-13 20:27:08 -0300

    Urna

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos:[
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'wiki.PNG', legenda:'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'wiki.PNG', legenda:'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'wiki.PNG', legenda:'Prefeito'},
+                    {url:'wiki.PNG', legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Azul',
+                fotos:[
+                    {url:'wiki.PNG', legenda:'Prefeito'},
+                    {url:'wiki.PNG', legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+            
+        ]
+    }
+]
